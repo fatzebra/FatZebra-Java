0a830589f55edca8955cce5097260405fd4be771
Matthew Savage <matt@amasses.net>
2018-03-06 14:32:08 +1100

    Bump lib version to 1.6

--- src/au/com/fatzebra/javalib/FatZebra.java
+++ src/au/com/fatzebra/javalib/FatZebra.java
@@ -7,7 +7,7 @@ public class FatZebra {
     /**
      * The Library version
      */
-    public static final String VERSION = "1.5";
+    public static final String VERSION = "1.6";
 
     /**
      * The authentication username for the gateway
